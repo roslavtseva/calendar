14f09054a097992b16013b2d3202f7d5c70c272e
Aleksey <aleksey_naumenko@ukr.net>
2019-12-20 10:08:25 +0200

    render calendar

---
+++ src/scripts/render-calendar.js
@@ -0,0 +1,21 @@
+const calendar = document.querySelector('.calendar');
+
+const renderCalendar = () => {
+    const week = document.createElement('div');
+    week.classList.add('week');
+    calendar.append(week);
+
+    for (let i = 1; i <= 7; i++) {
+        const day = document.createElement('div');
+        day.classList.add('day');
+        week.append(day);
+
+        for (let j = 1; j <= 24; j++) {
+            const hour = document.createElement('div');
+            hour.classList.add('hour');
+            day.append(hour);
+        }
+    }
+}
+
+renderCalendar();
