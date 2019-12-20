1026681a262dbe3cb4c24650e8bf51d3841bad30
Macbook Pro <nastyarosl@gmail.com>
2019-12-20 12:38:16 +0200

    delete trash

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,27 +1,53 @@
-const sidebarHTML = document.querySelector(".sidebar");
-const dayHours = 24;
 
-const renderSidebar = hourItems => {
-
+function renderSidebar () {
+  const sidebar = document.querySelector('.sidebar');
+  const dayHours = [];
   
-  const itemsHour = hourItems
-  .map((el) => {
-    for (hour = 0; hour < dayHours; hour++) {
-
-      const sidebarHour = document.createElement("div");
-      sidebarHour.classList.add("sidebar__hour");
-
-      const sidebarHourText = document.createElement("span");
-      sidebarHourText.classList.add("sidebar__hour-text");
-
-      sidebarHourText.value = hour;
-
-      sidebarHour.append(sidebarHourText);
-      return sidebarHour;
-    }
-  });
-
-  sidebarHTML.append(...itemsHour);
+  for (let hour = 0; hour < 24; hour++) {
+    
+    const sidebarHour = document.createElement('div');
+    sidebarHour.classList.add('sidebar__hour');
+    
+    const sidebarHourText = document.createElement('span');
+    sidebarHourText.classList.add('sidebar__hour-text');
+    
+    sidebarHourText.textContent = `${(hour < 10) ? '0' + hour : hour}:00`;
+    
+    sidebarHour.append(sidebarHourText);
+    dayHours.push(sidebarHour);
+  }
+  
+  sidebar.append(...dayHours);
 };
 
-console.log(renderSidebar());
+//export { renderSidebar };
+
+
+
+// const sidebarHTML = document.querySelector(".sidebar");
+// const dayHours = 24;
+
+// const renderSidebar = hourItems => {
+
+  
+//   const itemsHour = hourItems
+//   .map((el) => {
+//     for (hour = 0; hour < dayHours; hour++) {
+
+//       const sidebarHour = document.createElement("div");
+//       sidebarHour.classList.add("sidebar__hour");
+
+//       const sidebarHourText = document.createElement("span");
+//       sidebarHourText.classList.add("sidebar__hour-text");
+
+//       sidebarHourText.value = hour;
+
+//       sidebarHour.append(sidebarHourText);
+//       return sidebarHour;
+//     }
+//   });
+
+//   sidebarHTML.append(...itemsHour);
+// };
+
+// console.log(renderSidebar());
