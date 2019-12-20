1308eed2debd8d906bcc3bca3cb20cb3be364aca
Aleksey <aleksey_naumenko@ukr.net>
2019-12-20 15:38:14 +0200

    last stage

--- src/scripts/render-calendar.js
+++ src/scripts/render-calendar.js
@@ -1,18 +1,16 @@
 const calendar = document.querySelector('.calendar');
 
-const renderCalendar = () => {
+export const renderCalendar = () => {
     const week = document.createElement('div');
     week.classList.add('calendar__week-bar');
     calendar.append(week);
 
     for (let i = 1; i <= 7; i++) {
-        console.log('1');
         const day = document.createElement('div');
         day.classList.add('calendar__day-bar');
         week.append(day);
 
         for (let j = 1; j <= 24; j++) {
-            console.log('2');
             const hour = document.createElement('div');
             hour.classList.add('calendar__hour-bar');
             day.append(hour);
@@ -20,7 +18,6 @@ const renderCalendar = () => {
     }
 }
 renderCalendar();
-console.log('LOL');
 
 
 
