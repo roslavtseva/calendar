db2894609777d808b68a4ddb7f903fb4cc72ef1e
Macbook Pro <nastyarosl@gmail.com>
2019-12-24 14:12:10 +0200

    add color chooser+ rename 'date-end'

---
+++ src/scripts/createPopUp.js
@@ -0,0 +1,17 @@
+
+
+
+const popup = document.querySelector('.popup');
+const createButton = document.querySelector('.header__button_create');
+const createEventOnPopup = document.querySelector('.popup__action_create');
+
+
+const formFieldPopUp = {
+    name: document.querySelector('.popup__title-input'),
+    dateStart: document.querySelector('.popup__info_date-created'),
+    dateEnd: document.querySelector('.popup__info_date-end'),
+    timeStart: document.querySelector('.popup__info_duration-from'),
+    timeEnd: document.querySelector('.popup__info_duration-to'),
+    description: document.querySelector('.popup__description'),
+    color: document.querySelector('.event__color-picker'),
+};
