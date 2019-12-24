d26b0abe77e34742d990acb02ea7e12d5ccaf951
Aleksey <aleksey_naumenko@ukr.net>
2019-12-24 14:37:06 +0200

    fixing buttons

--- src/scripts/display-current-week.js
+++ src/scripts/display-current-week.js
@@ -1,69 +1,116 @@
-import { week } from './storage.js';
 import { createDaysOfWeek } from './render-week.js';
 
 export { displayCurrentWeek };
 export { switchWeekForward };
 export { switchWeekBackward };
+export { displayMonth };
+
+export const currentWeek = [
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date(),
+    new Date()
+];
+
+const forwardSwitcherBtn = document.querySelector('.header__week-toggle_chevron-right');
+const backwardSwitcherBtn = document.querySelector('.header__week-toggle_chevron-left');
+const monthAndYear = document.querySelector('.header__current-month-year');
 
 
 const displayCurrentWeek = (week) => {
     const currentDate = new Date().getDate();
     const currentDay = new Date().getDay();
-
+    let counterPrev = currentDay;
+    let counterNext = 1;
     for (let i = 0; i < week.length; i++) {
 
         if (i < currentDay) {
-            let date = currentDate - (i + 1);
-            week[i].date = new Date(new Date().setDate(date));
+            let date = currentDate - counterPrev;
+            week[i] = new Date(new Date().setDate(date));
+            counterPrev--;
         } else if (i == currentDay) {
-            week[i].date = new Date();
+            week[i] = new Date();
         } else {
-            let date = currentDate + (i - 1);
-            week[i].date = new Date(new Date().setDate(date));
+            let date = currentDate + counterNext;
+            week[i] = new Date(new Date().setDate(date));
+            counterNext++;
         }
     }
-    createDaysOfWeek(week);
+    displayMonth(currentWeek);
+    createDaysOfWeek(currentWeek);
 };
 
-displayCurrentWeek(week);
+displayCurrentWeek(currentWeek);
+console.log(currentWeek);
 
 
 
 const todayBtn = document.querySelector('.header__button_today');
 const todayWeekSwitcher = () => {
-    displayCurrentWeek(week);
+    displayCurrentWeek(currentWeek);
 };
 const switchToTodaysWeek = todayBtn.addEventListener('click', todayWeekSwitcher);
 
 
 
 
-const forwardSwitcherBtn = document.querySelector('.header__week-toggle_chevron-right');
-function forwardSwitcher(week) {
-    let newWeek = [...week];
+function forwardSwitcher(currentWeek) {
+    let newWeek = [...currentWeek];
 
-    newWeek.map(day => {
-        const newDate = day.date;
-        const newDay = newDate.getDate();
+    newWeek.map(dateOfDay => {
+        const newDate = dateOfDay.getDate();
 
-        day.date = new Date(new Date(newDate).setDate(newDay + 7));
+        dateOfDay = new Date(dateOfDay.setDate(newDate + 7));
     });
-    createDaysOfWeek(week);
+    displayMonth(currentWeek);
+    createDaysOfWeek(currentWeek);
 };
-const switchWeekForward = forwardSwitcherBtn.addEventListener('click', forwardSwitcher.bind(forwardSwitcherBtn, week));
+const switchWeekForward = forwardSwitcherBtn.addEventListener('click', forwardSwitcher.bind(forwardSwitcherBtn, currentWeek));
 
 
 
-const backwardSwitcherBtn = document.querySelector('.header__week-toggle_chevron-left');
-const backwardSwitcher = (week) => {
-    let newWeek = [...week];
+const backwardSwitcher = (currentWeek) => {
+    let newWeek = [...currentWeek];
 
-    newWeek.map(day => {
-        const newDate = day.date;
-        const newDay = newDate.getDate();
+    newWeek.map(dateOfDay => {
+        const newDate = dateOfDay.getDate();
 
-        day.date = new Date(new Date(newDate).setDate(newDay - 7));
+        dateOfDay = new Date(dateOfDay.setDate(newDate - 7));
     });
-    createDaysOfWeek(week);
+    displayMonth(currentWeek);
+    createDaysOfWeek(currentWeek);
 };
-const switchWeekBackward = backwardSwitcherBtn.addEventListener('click', backwardSwitcher.bind(backwardSwitcherBtn, week));
+const switchWeekBackward = backwardSwitcherBtn.addEventListener('click', backwardSwitcher.bind(backwardSwitcherBtn, currentWeek));
+
+
+
+function displayMonth(week) {
+    let arrMonth = [];
+    let arrYear = [];
+    let result;
+    for (let i = 0; i < week.length; i++) {
+
+        let month = week[i].toDateString().split(' ')[1];
+        let year = week[i].toDateString().split(' ')[3];
+
+        if (arrMonth.indexOf(month) == -1) {
+            arrMonth.push(month);
+        };
+        if (arrYear.indexOf(year) == -1) {
+            arrYear.push(year);
+        }
+    }
+    if (arrMonth.length == 1) {
+        result = `${arrMonth[0]} ${arrYear[0]}`;
+    }
+    if (arrMonth.length == 2) {
+        result = `${arrMonth[0]} - ${arrMonth[1]} ${arrYear[0]}`;
+    }
+    if (arrYear.length == 2) {
+        result = `${arrMonth[0]} ${arrYear[0]} - ${arrMonth[1]} ${arrYear[1]}`;
+    }
+    monthAndYear.innerHTML = result;
+}
--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -1,9 +1,1 @@
-export const week = [
-    {name: 'Sun', date: new Date()},
-    {name: 'Mon', date: new Date()},
-    {name: 'Tue', date: new Date()},
-    {name: 'Wed', date: new Date()},
-    {name: 'Thu', date: new Date()},
-    {name: 'Fri', date: new Date()},
-    {name: 'Sat', date: new Date()},
-];
+
