b8e9d85a1e32242c7cf3b0cf9a31324172a160bc
Macbook Pro <nastyarosl@gmail.com>
2019-12-25 13:23:37 +0200

    debug styles in popup

--- src/scripts/create-popup.js
+++ src/scripts/create-popup.js
@@ -36,3 +36,22 @@ function showSavedEvent(event) {
     formFieldPopUp.description.value = selectedEvent.description;
     formFieldPopUp.color.value = selectedEvent.color;
 };
+
+function dateToString(date) {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    month = month < 10 ? '0' + month : month;
+    let day = date.getDate();
+    day = day < 10 ? '0' + day : day;
+
+    return `${year}-${month}-${day}`;
+};
+
+function timeToString(date) {
+    let hour = date.getHours();
+    hour = hour < 10 ? '0' + hour : hour;
+    let minutes = date.getMinutes();
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    return `${hour}:${minutes}`;
+}
